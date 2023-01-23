0416cd02b877c4e75ee7199c246c3679aa22e2dc
Reshxram <74835069+Reshxram@users.noreply.github.com>
2023-01-23 21:13:33 +0100

    PROGRESS-BAR

--- data/move_files.py
+++ data/move_files.py
@@ -1,16 +1,17 @@
 import shutil
 import numpy as np
 import os
+from tqdm import PROGRAtqdm
 
 wkdir = os.path.dirname(os.path.realpath(__file__))
 
 x_train = np.load(wkdir + "\\x_train_filenames.npy")
 x_test = np.load(wkdir + "\\x_test_filenames.npy")
 
-for file in x_train:
+for file in tqdm(x_train):
     label = file.split("-")[0]
     shutil.copyfile(wkdir + "\\all\\" + file, wkdir + f"\\train\\{label}\\{file}")
 
-for file in x_test:
+for file in tqdm(x_test):
     label = file.split("-")[0]
     shutil.copyfile(wkdir + "\\all\\" + file, wkdir + f"\\test\\{label}\\{file}")
