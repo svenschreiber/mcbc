f530ebf9be3aa42ac6ad3694643398405844aef6
Reshxram <74835069+Reshxram@users.noreply.github.com>
2023-01-23 21:10:09 +0100

    update

--- data/move_files.py
+++ data/move_files.py
@@ -4,13 +4,13 @@ import os
 
 wkdir = os.path.dirname(os.path.realpath(__file__))
 
-x_train = np.load(wkdir + "x_train_filenames.npy")
-x_test = np.load(wkdir + "x_test_filenames.npy")
+x_train = np.load(wkdir + "\\x_train_filenames.npy")
+x_test = np.load(wkdir + "\\x_test_filenames.npy")
 
 for file in x_train:
     label = file.split("-")[0]
-    shutil.copyfile(wkdir + "all\\" + file, wkdir + f"train\\{label}\\{file}")
+    shutil.copyfile(wkdir + "\\all\\" + file, wkdir + f"\\train\\{label}\\{file}")
 
 for file in x_test:
     label = file.split("-")[0]
-    shutil.copyfile(wkdir + "all\\" + file, wkdir + f"test\\{label}\\{file}")
+    shutil.copyfile(wkdir + "\\all\\" + file, wkdir + f"\\test\\{label}\\{file}")
